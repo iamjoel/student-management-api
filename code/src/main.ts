dea4ab7925511476be6267812cb547a7e510535d
金伟强 <jinweiqiang@coding.net>
2022-07-15 16:21:22 +0800

    swagger

--- code/src/main.ts
+++ code/src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api'); // 加全局路由的前缀
+
+  //  Swagger start
+  const options = new DocumentBuilder()
+    .setTitle('植物管理系统')
+    .setDescription('管理我的植物。')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-doc', app, document); // 访问文档地址：http://127.0.0.1:3010/api-doc
+  //  Swagger end
 
   await app.listen(3010);
 }
--- code/src/module/student/student.controller.ts
+++ code/src/module/student/student.controller.ts
@@ -8,34 +8,51 @@ import {
   Delete,
   Body,
 } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiQuery, ApiResponse, ApiParam, ApiProperty } from '@nestjs/swagger';
 import { RO } from '../../declarations/service';
 
 import { StudentService } from './student.service';
 
-interface Student {
+class Student {
+  @ApiProperty({
+    description: '名称',
+    default: '',
+  })
   name: string
 }
 
+@ApiTags('学生')
 @Controller('student')
 export class StudentController {
   constructor(private readonly service: StudentService) {}
 
   @Get('')
+  @ApiOperation({ summary: '列表' })
+  @ApiQuery({ name: 'name', description: '姓名', schema: { default: '' } })
+  @ApiResponse({ status: 200, description: '操作成功' })
   list(@Query('name') name: string): RO {
     return this.service.list(name)
   }
 
   @Get(':id')
+  @ApiOperation({ summary: '详情' })
+  @ApiParam({ name: 'id', description: 'id' , schema: { default: 1 } })
+  @ApiResponse({ status: 200, description: '操作成功' })
   detail(@Param() params: { id: string }): RO {
     return this.service.detail(parseInt(params.id, 10))
   }
 
   @Post('')
+  @ApiOperation({ summary: '创建' })
+  @ApiResponse({ status: 200, description: '操作成功' })
   create(@Body() student: Student) {
     return this.service.create(student.name)
   }
 
   @Put(':id')
+  @ApiOperation({ summary: '编辑' })
+  @ApiParam({ name: 'id', description: 'id' , schema: { default: 1 } })
+  @ApiResponse({ status: 200, description: '操作成功' })
   update(
     @Param() params: { id: string },
     @Body() student: Student
@@ -44,6 +61,8 @@ export class StudentController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: '删除' })
+  @ApiResponse({ status: 200, description: '操作成功' })
   delete(@Param() params: { id: string }): RO {
     return this.service.delete(parseInt(params.id, 10))
   }
