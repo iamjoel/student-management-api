821ee5798eeb13dd935aeaeda4696365095ad2c3
金伟强 <jinweiqiang@coding.net>
2022-07-20 14:26:13 +0800

    upload

--- code/src/app.controller.ts
+++ code/src/app.controller.ts
@@ -1,7 +1,46 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
+import { ApiTags, ApiConsumes, ApiBody } from '@nestjs/swagger';
+import { RO } from './declarations/service';
+import { handler } from './utils/handler';
 import { AppService } from './app.service';
 
+import * as fs from 'fs-extra';
+
+@ApiTags('通用')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  // 文件上传
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  uploadFile(@UploadedFile() file: Express.Multer.File): Promise<RO> {
+    return handler(() => {
+      const fileName = `${Date.now()}-${file.originalname}`; // 会写到项目根目录
+      const filePath = `upload/files/${fileName}`;
+      fs.outputFileSync(filePath, file.buffer, 'utf8');
+      return {
+        file: fileName,
+      };
+    });
+  }
 }
--- code/src/app.module.ts
+++ code/src/app.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as appRoot from 'app-root-path';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentModule } from './module/student/student.module';
@@ -10,6 +13,15 @@ import { IdInfoModule } from './module/id-info/id-info.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
+     // 配置 https://github.com/nestjs/serve-static/blob/master/lib/interfaces/serve-static-options.interface.ts
+     ServeStaticModule.forRoot({
+      // 静态服务器。 server 上传的文件用。
+      rootPath: join(appRoot.path, 'upload'), // 文件夹路径
+      exclude: ['/api*'],
+      serveStaticOptions: {
+        index: false,
+      },
+    }),
     StudentModule,
     PetModule,
     TeacherModule,
